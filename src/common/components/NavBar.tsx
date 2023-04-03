1d0871a2f10f801340d66f0bae224e7e095d4d2a
alex-priiv <alex.shukhman@theprivacyco.com>
2023-04-03 17:11:12 -0400

    Make use easier

--- src/common/components/NavBar.tsx
+++ src/common/components/NavBar.tsx
@@ -1,15 +1,23 @@
+import { Button as LinkButton } from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
 
 export function NavBar () {
+  const [checked, setChecked] = useState(false);
+  
+  function unToggle () {
+    setChecked(false);
+  }
+  
   return (
     <section className="top-nav">
-      <input id="menu-toggle" type="checkbox" />
+      <input id="menu-toggle" type="checkbox" checked={checked} onChange={(e) => setChecked(e.target.checked)}/>
       <label className='menu-button-container' htmlFor="menu-toggle">
       <div className='menu-button'></div>
       </label>
-      <ul className="menu">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/where'>Where</Link></li>
+      <ul className="menu" onClick={unToggle}>
+        <li><Link to='/'><LinkButton>Home</LinkButton></Link></li>
+        <li><Link to='/where'><LinkButton >Where</LinkButton></Link></li>
         <li>When</li>
         <li>Details</li>
         <li>Registry</li>
