fc76979b2b43009f57f6f3a845bb40b4af207b21
alex-priiv <alex.shukhman@theprivacyco.com>
2023-04-03 15:17:35 -0400

    use hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import {setupClients} from "app/axiosClinets";
 import {persistor, store} from "app/store";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {App} from "app";
 import {NavBar} from 'common/components'
 
@@ -20,10 +20,10 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate persistor={persistor}>
-                <BrowserRouter>
+                <HashRouter>
                     <NavBar/>
                     <App/>
-                </BrowserRouter>
+                </HashRouter>
             </PersistGate>
         </Provider>
     </React.StrictMode>
