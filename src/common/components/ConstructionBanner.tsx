1efd0135b88c72ba31efc83ca611588cf57ec755
alex-priiv <alex.shukhman@theprivacyco.com>
2023-04-04 18:19:40 -0400

    Add files

---
+++ src/common/components/ConstructionBanner.tsx
@@ -0,0 +1,9 @@
+export function ConstructionBanner () {
+  return (
+    <div className='bannerHolder'>
+      <div className='constructionBanner'>
+        UNDER CONSTRUCTION
+      </div>
+    </div>
+  )
+}
